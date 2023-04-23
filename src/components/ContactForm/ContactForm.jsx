7fe35038fed711d08ce619a490590c7a855ed27b
Meghan Bucher <115611931+bardIRL@users.noreply.github.com>
2023-04-23 16:19:30 -0700

    adds reCAPTCHA v2 to contact form, adds notification banners

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,40 +1,52 @@
-import { useRef } from "react";
-import emailjs from "@emailjs/browser";
 import "./ContactForm.css";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import ReCAPTCHA from "react-google-recaptcha";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function ContactForm() {
   const form = useRef();
+  const [recaptchaVerified, setRecaptchaVerified] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        process.env.REACT_APP_EMAILJS_SERVICE_ID,
-        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
-        form.current,
-        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+    if (recaptchaVerified) {
+      emailjs
+        .sendForm(
+          process.env.REACT_APP_EMAILJS_SERVICE_ID,
+          process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+          form.current,
+          process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+        )
+        .then(
+          (result) => {
+            toast.success("Message sent successfully! 👌");
+          },
+          (error) => {
+            toast.error("Failed to send message. 😕");
+          }
+        );
+    } else {
+      toast.warning("Please verify reCAPTCHA. 🤖");
+    }
   };
+
+  function onChange(value) {
+    setRecaptchaVerified(!!value);
+  }
 
   return (
     <form ref={form} onSubmit={sendEmail}>
       <div className="row">
         <div className="col">
           <label>First Name *</label>
-          <input type="text" name="user_firstname" />
+          <input required type="text" name="user_firstname" />
         </div>
         <div className="col">
           <label>Last Name *</label>
-          <input type="text" name="user_lastname" />
+          <input required type="text" name="user_lastname" />
         </div>
       </div>
       <div className="row">
@@ -44,16 +56,22 @@ export default function ContactForm() {
         </div>
         <div className="col">
           <label>Email *</label>
-          <input type="email" name="user_email" />
+          <input required type="email" name="user_email" />
         </div>
       </div>
       <div className="col">
         <label>Subject *</label>
-        <input type="text" name="user_subject" />
+        <input required type="text" name="user_subject" />
         <label>Message *</label>
-        <textarea rows="4" name="user_message" />
+        <textarea required rows="4" name="user_message" />
+        <ReCAPTCHA
+          className="recaptcha"
+          sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
+          onChange={onChange}
+        />
         <input type="submit" value="Send Message" />
       </div>
+      <ToastContainer />
     </form>
   );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,5 @@
 import "./Footer.css";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
 import SocialIcons from "../SocialIcons/SocialIcons";
-
-library.add(faGithub, faLinkedin, faEnvelope);
 
 export default function Footer() {
   return (
