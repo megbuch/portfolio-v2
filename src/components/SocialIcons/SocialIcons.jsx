4b6c7e29d470cfd52c459def44f87e9d74edea9d
Meghan Bucher <115611931+bardIRL@users.noreply.github.com>
2023-04-24 16:17:41 -0700

    fixes links in social icons component

--- src/components/SocialIcons/SocialIcons.jsx
+++ src/components/SocialIcons/SocialIcons.jsx
@@ -3,7 +3,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
 import "./SocialIcons.css";
 
 library.add(faGithub, faLinkedin, faEnvelope);
